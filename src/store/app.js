0e666b1bbe1d70899eddced06bf80742739e010d
BraNFT™ <99556664+branfts@users.noreply.github.com>
2024-07-25 15:49:35 -0700

    Initial commit

---
+++ src/store/app.js
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia"
+
+const reset = {
+    form: {
+        name: undefined,
+        names: [],
+        links: [{
+            url: ''
+        }],
+        rules: [],
+    }
+}
+export const useAppStore = defineStore("app", {
+    state: () => ({
+        faq: {},
+        analytics: {
+        },
+        form: JSON.parse(JSON.stringify(reset.form)),
+        stats: {}
+    }),
+    actions: {
+        resetForm(state) {
+            state.form = JSON.parse(JSON.stringify(reset.form))
+        },
+    },
+    persist: true,
+})
