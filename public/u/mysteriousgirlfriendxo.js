05d278dcac89b9a63f30ad0635309fee8ab9b050
branfts <support@june07.com>
2024-07-25 15:49:38 -0700

    Updated by A10x Service

---
+++ public/u/mysteriousgirlfriendxo.js
@@ -0,0 +1,20 @@
+
+const links = [
+    {
+        "url": "https://www.tiktok.com/@mysteriusgirlfriendxo"
+    },
+    {
+        "url": "https://fansly.com/mysteriousgirlfriend"
+    },
+    {
+        "url": "https://www.reddit.com/r/mysteriousgfxoxo/"
+    },
+    {
+        "url": "https://www.instagram.com/mysteriousgirlfriendxoxo/?hl=en"
+    },
+    {
+        "url": "https://www.instagram.com/mysteriousgirlfriendxo/?hl=en"
+    }
+];
+const rules = [];
+export { links, rules };
