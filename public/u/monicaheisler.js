be9cbf5d723ff9424865b233b216a7d170ad96c1
branfts <support@june07.com>
2024-07-29 16:22:18 -0700

    Updated by A10x Service

---
+++ public/u/monicaheisler.js
@@ -0,0 +1,16 @@
+
+const links = [
+    { url: 'mailto:monicalheisler@gmail.com' },
+{ url: 'https://tiktok.com/@backtocenter' },
+{ url: 'https://instagram.com/monicaheisler' },
+{ url: 'http://desertslumber.com/' },
+{ url: 'https://littleleaders.godaddysites.com/' },
+{ url: 'https://www.linkedin.com/in/monica-heisler/' },
+{ url: 'https://monicaheisler.my.canva.site/monica-heisler-creative-portfolio' },
+{ url: 'https://linktr.ee/monicaheisler' }
+]
+const rules = [
+    
+]
+
+export { links, rules }
